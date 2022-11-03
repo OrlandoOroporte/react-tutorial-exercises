4c2069b693d472a254be75ca5d49006c85f21a7b
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-11-03 22:38:48 +0000

    all test pass

--- .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
+++ .learn/resets/02.2-maping-array-of-objects-to-li/app.jsx
@@ -7,9 +7,10 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
-let list = planets.map((item) => <li className="list-group-item">{item}</li>);
+
+
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
-const content = <ul className="list-group m-5">{list}</ul>;
+const content = (<ul className="list-group m-5"></ul>);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- exercises/01.4-building-html-from-arrays/app.jsx
+++ exercises/01.4-building-html-from-arrays/app.jsx
@@ -2,8 +2,17 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const navlinkItems = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
+const itemLink = navlinkItems.map((link) => (
+	<a className="nav-link" href="#" >
+        {link}
+	</a>
+));
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+const content = (
+	<ul className="nav">
+		<li className="nav-item">{itemLink}</li>
+	</ul>
+);
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
